89660cda8f524be1b82a73a709283c60447be1fa
rzolotarev <mirom90@gmail.com>
2020-05-03 12:29:46 +0200

    combination of k from n

---
+++ CombinationsOfKFromN/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CombinationsOfKFromN
+{
+    class Program
+    {
+        private static Dictionary<Pair, int> cache = new Dictionary<Pair, int>();
+
+        static void Main(string[] args)
+        {
+            var number = Combinations(100, 5);
+            Console.WriteLine($"number of combinations {number}");
+        }
+
+        private static int Combinations(int n, int k)
+        {
+            if (n < k)
+                return 0;
+
+            if (n == k)
+                return 1;
+            if (k == 1)
+                return n;
+
+            var pair = new Pair() {K = k, N = n};
+            if (cache.ContainsKey(pair))
+                return cache[pair];
+
+            var withFixedElement = Combinations(n-1, k-1);
+            cache.TryAdd(new Pair() { K = k - 1, N = n - 1}, withFixedElement);
+            var withoutFixedElement = Combinations(n - 1, k);
+            cache.TryAdd(new Pair() { K = k, N = n - 1 }, withoutFixedElement);
+            return withoutFixedElement + withFixedElement;
+        }
+
+        struct Pair
+        {
+            public int N { get; set; }
+            public int K { get; set; }
+        }
+    }
+}
---
+++ CombinationsOfKFromN/ReadMe.txt
@@ -0,0 +1,5 @@
+﻿We can apply reccurent formula with caching
+C(n,k) = C(n-1, k-1) + C(n-1, k) for combinations without repetition
+
+C(n, k) = 1 // n = k
+C(n, 1) = n
