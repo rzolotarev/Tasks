d8e6da7aadd9805cbd884d6752e2406c909133fc
rzolotarev <mirom90@gmail.com>
2020-05-03 09:48:28 +0200

    number of rectangles in plain

---
+++ RectanglesFromPoints/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RectanglesFromPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = new List<Point>()
+            {
+                new Point(0,0),
+                new Point(0,1),
+                new Point(0,2),
+                new Point(1,0),
+                new Point(1,1),
+                new Point(1,2),
+                new Point(2,0),
+                new Point(2,1),
+                new Point(2,2)
+            };
+
+            var number = GetNumberOfRectangles(points);
+            Console.WriteLine($"{number} of rectangles");
+        }
+
+        private static int GetNumberOfRectangles(List<Point> points)
+        {
+            var number = 0;
+            var pairSet = new Dictionary<PairY, int>(); 
+            foreach (var point1 in points)
+                foreach (var point2 in points)
+                {
+                    if (point1.X == point2.X && point1.Y < point2.Y)
+                    {
+                        var pair = new PairY(point1.Y, point2.Y);
+                        pairSet.TryAdd(pair, 0);
+                        number += pairSet[pair]; // calculating number of all combinations of vertical pairs
+                        pairSet[pair]++;
+                    }
+                }
+
+            return number;
+        }
+
+
+        struct PairY
+        {
+            private int Y1 { get; set; }
+            private int Y2 { get; set; }
+
+            public PairY(int y1, int y2)
+            {
+                Y1 = y1;
+                Y2 = y2;
+            }
+        }
+        
+        class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
---
+++ RectanglesFromPoints/ReadMe.txt
@@ -0,0 +1,31 @@
+﻿
+								x3y2
+		        x1y1    x2y1	x3y1
+                x1y0    x2y0	x3y0
+
+If we group by x coordinates we will get all points belonging to the one x.
+				x1: [y0, y1]
+				x2: [y0, y1]
+				x3: [y0, y1, y2]
+What do we see? We can form a rectangular if points have the same "y" coordinates, the same pair of 
+"y" coordinates on different "x" coordinates.
+
+For such combination we can form one rectangular;
+y1  y1
+y0  y0
+
+For such combination we can form 3 rectangulars;
+y1  y1 y1
+y0  y0 y0           
+
+It's a formula from combinatoric - all combinations of k(2) from n (3). 
+
+C(2,2) = C(1,1) + C(1,2) = 1 + 0 = 1;
+
+C(3,2) = C(2,2) + C(2, 1) = 1 + 2 = 3; C(2,1) = count of verticle lines - 1
+C(2,2) - is stored in number variables;
+
+y1  y1 y1 y1
+y0  y0 y0 y0          
+
+C(4,2) = C(3,2) + C(3,1) = 3 + 3; C(3,1) = count of verticle lines - 1
