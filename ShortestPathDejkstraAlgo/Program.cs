a8caebdc4c3778c1e95b380b01180578b2daba26
rzolotarev <mirom90@gmail.com>
2020-05-05 10:57:40 +0200

    heap sort

--- ShortestPathDejkstraAlgo/Program.cs
+++ ShortestPathDejkstraAlgo/Program.cs
@@ -1,12 +1,118 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace ShortestPathDejkstraAlgo
+class Solution
 {
-    class Program
+
+    class Node
     {
-        static void Main(string[] args)
+        public int Index;
+        public int Path;
+        public int PrecedingNode;
+        public bool Visited;
+    }
+
+    // Complete the shortestReach function below.
+    static int[] shortestReach(int n, int[][] edges, int s)
+    {
+
+        // initializing
+        var adjacencyList = new LinkedList<Node>[n];
+        var mapList = new Node[n]; // list of shortest paths from starting point to any
+        for (var i = 0; i < n; i++)
         {
-            Console.WriteLine("Hello World!");
+            adjacencyList[i] = (new LinkedList<Node>());
+            mapList[i] = (new Node() { Path = int.MaxValue, PrecedingNode = -1 });
         }
+        mapList[s - 1] = new Node() { Path = 0, PrecedingNode = -1 };
+
+        // built adjacency list
+
+        foreach (var currentEdges in edges)
+        {
+            adjacencyList[currentEdges[0] - 1]
+                .AddLast(new Node()
+                {
+                    Index = currentEdges[1] - 1,
+                    Path = currentEdges[2]
+                });
+            adjacencyList[currentEdges[1] - 1]
+                .AddLast(new Node()
+                {
+                    Index = currentEdges[0] - 1,
+                    Path = currentEdges[2]
+                });
+        }
+
+        var queue = new Queue<int>();
+        queue.Enqueue(s - 1); // starting point
+
+        while (queue.Count > 0)
+        {
+            var i = queue.Dequeue();
+            // go through adjacent nodes and calculate the shortes path to any node
+            foreach (var adjNode in adjacencyList[i])
+            {
+                if (adjNode.Path + mapList[i].Path < mapList[adjNode.Index].Path)
+                {
+                    mapList[adjNode.Index].Path = adjNode.Path + mapList[i].Path;
+                    mapList[adjNode.Index].PrecedingNode = i;
+                }
+
+                if (mapList[adjNode.Index].Visited == true)
+                    continue;
+
+                queue.Enqueue(adjNode.Index);
+            }
+
+            mapList[i].Visited = true;
+        }
+
+        return mapList.Where(node => node.Path != 0).Select(node => node.Path == Int32.MaxValue ? -1 : node.Path).ToArray();
+    }
+
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@"C:\temp\1.txt", true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] nm = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nm[0]);
+
+            int m = Convert.ToInt32(nm[1]);
+
+            int[][] edges = new int[m][];
+
+            for (int i = 0; i < m; i++)
+            {
+                edges[i] = Array.ConvertAll(Console.ReadLine().Split(' '), edgesTemp => Convert.ToInt32(edgesTemp));
+            }
+
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            int[] result = shortestReach(n, edges, s);
+
+            textWriter.WriteLine(string.Join(" ", result));
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
+
---
+++ РуфзЫщке/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace РуфзЫщке
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var heap = new CustomQueue();
+            var random = new Random();
+            for(var i = 0; i < 100; i++)
+                heap.Add(new Node() {Index = i, Path = random.Next(-320, 320)});
+
+            while (heap.Count > 0)
+            {
+                Console.WriteLine(heap.Get().Path);
+            }
+        }
+    }
+
+    public class CustomQueue
+    {
+        private List<Node> queue = new List<Node>();
+        public int Count;
+
+        public void Add(Node node)
+        {
+            if (queue.Count > Count)
+                queue[Count] = node;
+            else
+                queue.Add(node);
+
+            Count++;
+
+            Reorder();
+        }
+
+        private void Reorder()
+        {
+            for (var i = Count / 2 - 1; i >= 0; i--)
+            {
+                if (2 * i + 2 == Count)
+                {
+                    if (queue[2 * i + 1].Path < queue[i].Path)
+                    {
+                        var temp = queue[i];
+                        queue[i] = queue[2 * i + 1];
+                        queue[2 * i + 1] = temp;
+                    }
+                }
+                else
+                {
+
+                    if (queue[2 * i + 2].Path < queue[2 * i + 1].Path)
+                    {
+                        if (queue[2 * i + 2].Path < queue[i].Path)
+                        {
+                            var temp = queue[i];
+                            queue[i] = queue[2 * i + 2];
+                            queue[2 * i + 2] = temp;
+                        }
+                    }
+                    else
+                    {
+                        if (queue[2 * i + 1].Path < queue[i].Path)
+                        {
+                            var temp = queue[i];
+                            queue[i] = queue[2 * i + 1];
+                            queue[2 * i + 1] = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+        public Node Get()
+        {
+            var min = queue[0];
+            queue[0] = queue[Count - 1];
+            Count--;
+            Reorder();
+            return min;
+        }
+    }
+
+    public class Node
+    {
+        public int Index;
+        public int Path;
+        public int PrecedingNode;
+        public bool Visited;
+    }
+}
