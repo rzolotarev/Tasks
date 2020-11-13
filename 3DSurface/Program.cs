cced53af6ed77654ba4aad19ff6754e3fa1c9973
rzolotarev <mirom90@gmail.com>
2020-11-13 17:26:30 +0100

    feat: added 3DSurface project

---
+++ 3DSurface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3DSurface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
