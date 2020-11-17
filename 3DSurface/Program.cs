d4f749f2182a095c2495cdacc8af02273addcc3a
rzolotarev <mirom90@gmail.com>
2020-11-17 21:46:14 +0100

    feat: implemented by sum of delta

--- 3DSurface/Program.cs
+++ 3DSurface/Program.cs
@@ -14,67 +14,24 @@ namespace _3DSurface
         {        
             var height = A.Length;
             var width = A[0].Length;
-            var topDaownPair = height * width * 2;
-            var result = topDaownPair;
-          
-            var mostTollestH = new int[height];
-            var mostTollestW = new int[width];
-            var innerSurface = FillMax(mostTollestH, mostTollestW, A);
-            result += innerSurface;            
-            var currentSum = 0;
-            foreach(var item in mostTollestH) {
-                currentSum += item;
-            }
-            result+= 2 * currentSum;
-            currentSum = 0;
-            foreach(var item in mostTollestW) {
-                currentSum += item;
-            }
+            var topDownPair = height * width * 2; // calculated two areas
+            var result = topDownPair;
 
-            result+= 2 * currentSum;
+            for(int i = 0; i < height; i++) {
+                result += (A[i][0] + A[i][width - 1]);
+                for(int j = 1; j < width; j++){
+                    result += Math.Abs(A[i][j] - A[i][j-1]);
+                }
+            }
+            
+            for(int j = 0; j < width; j++) {
+                result += (A[0][j] + A[height - 1][j]);
+                for(int i = 1; i < height; i++){
+                    result += Math.Abs(A[i][j] - A[i-1][j]);
+                }
+            }            
+            
             return result;
-        }
-
-        static int FillMax(int[] mostTollestH, int[] mostTollestW, int[][] A)
-        {
-            var height = A.Length;
-            var width = A[0].Length;
-            var i = height;
-            var j = width;
-            var innerSurface = 0;
-            while ((i | j) > 0) 
-            {                
-                if (i > 0)
-                    i--;
-                if (j > 0)
-                    j--;
-
-                var max = 0;
-                for(var w = 0; w < width; w++)
-                {
-                    if ( w > 0 && w < width - 1 && mostTollestH[i] == 0)
-                    {
-                        if (A[i][w -1] > A[i][w] && A[i][w + 1] > A[i][w])
-                            innerSurface += 2 * (Math.Min(A[i][w + 1], A[i][w - 1]) - A[i][w]);
-                    }
-                    max = Math.Max(A[i][w], max);
-                }
-                mostTollestH[i] = max;
-
-                max = 0;
-                for(var h = 0; h < height; h++)
-                {
-                    if ( h > 0 && h < height - 1 && mostTollestW[j] == 0)
-                    {
-                        if (A[h - 1][j] > A[h][j] && A[h + 1][j] > A[h][j])
-                            innerSurface += 2 * (Math.Min(A[h - 1][j], A[h + 1][j]) - A[h][j]);
-                    }
-                    max = Math.Max(A[h][j], max);
-                }
-                mostTollestW[j] = max;
-            }
-
-            return innerSurface;
-        }
+        }        
     }
 }
