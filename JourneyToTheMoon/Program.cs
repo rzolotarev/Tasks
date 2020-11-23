7097973a066b6c6b2fc1e185213e5e31f688f4a0
rzolotarev <mirom90@gmail.com>
2020-11-23 23:08:07 +0100

    implemetation

--- JourneyToTheMoon/Program.cs
+++ JourneyToTheMoon/Program.cs
@@ -1,18 +1,42 @@
 ﻿using System;
+using UnionSets;
+using System.Linq;
 
 namespace JourneyToTheMoon
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
-        {
-            Console.WriteLine("");
+        {            
+            var n = 10;
+            var astronaut = new int[7][];            
+            astronaut[0] = new int[2] {0, 2};
+            astronaut[1] = new int[2] {1, 8};
+            astronaut[2] = new int[2] {1, 4};
+            astronaut[3] = new int[2] {2, 8};
+            astronaut[4] = new int[2] {2, 6};
+            astronaut[5] = new int[2] {3, 5};
+            astronaut[6] = new int[2] {6, 9};
+            Console.WriteLine(JourneyToMoon(n, astronaut));
         }
 
         static int JourneyToMoon(int n, int[][] astronaut) 
         {
+            // preparing disjoint set
+            var unionSet = new UnionSet(n);             
+            for(int i = 0; i < astronaut.Length; i++)            
+                unionSet.Union(astronaut[i][0], astronaut[i][1]);
+                          
+            // symmary of all countries
+            var sum = 0;
+            var countries = unionSet.Parents.GroupBy(p => p).Select(p => p.Count()).ToList<int>();            
+            // foreach(var rank in countries)
+            //     Console.WriteLine(rank);
+            for(int i = countries.Count() - 1; i > 0; i--)            
+                for(int j = i - 1; j >= 0; j--)                
+                    sum += countries[i] * countries[j];
 
+            return sum;
         }
-
     }
 }
---
+++ JourneyToTheMoon/UnionSet.cs
@@ -0,0 +1,69 @@
+
+using System;
+
+namespace UnionSets {
+
+    public class UnionSet
+    {
+        public int[] Ranks { get; set; }
+        public int[] Parents { get; set; }        
+        public int N { get; set; }
+
+        public UnionSet(int n)
+        {
+            Ranks = new int[n];
+            Parents = new int[n];            
+            N = n;
+            InitializeParents();
+        }
+
+        private void InitializeParents() 
+        {
+            for(int i = 0; i < N; i++)
+            {
+                Parents[i] = i;                
+            }            
+        }
+
+        public int FindRoot(int node)
+        {
+            if (node != Parents[node])
+            {                       
+                Parents[node] = FindRoot(Parents[node]);                
+            }
+
+            return Parents[node];
+        }
+
+        public void Union(int x, int y)
+        {
+            int xRoot = FindRoot(x);
+            int yRoot = FindRoot(y);
+
+            if (xRoot == yRoot)
+                return;
+            
+            if (Ranks[xRoot] > Ranks[yRoot])
+            {
+                Parents[yRoot] = xRoot;                
+            }
+            else 
+            {
+                if (Ranks[xRoot] < Ranks[yRoot]) 
+                {
+                    Parents[xRoot] = yRoot;                
+                }
+                else 
+                {
+                    Parents[yRoot] = xRoot;
+                    for(var i = 0; i < N; i++)
+                    {
+                        if (Parents[i] == yRoot)
+                            Parents[i] = xRoot;
+                    }
+                    Ranks[xRoot] = Ranks[xRoot] + 1;         
+                }
+            }                       
+        }
+    }
+}
