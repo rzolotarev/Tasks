8f0c0ff28c8ddd47c175bfd0ea4a10ae9fac10ae
rzolotarev <mirom90@gmail.com>
2020-11-23 20:22:15 +0100

    preparing project

---
+++ JourneyToTheMoon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JourneyToTheMoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+
+        static int JourneyToMoon(int n, int[][] astronaut) 
+        {
+
+        }
+
+    }
+}
