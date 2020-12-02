f51f533fd1dd4fe249ca5d5017f6170687c8b325
rzolotarev <mirom90@gmail.com>
2020-12-02 14:28:01 +0100

    feat: init even tree

---
+++ EventTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EventTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int evenForest(int t_nodes, int t_edges, List<int> t_from, List<int> t_to) 
+        {
+            if (t_nodes % 2 != 0)
+                throw new ArgumentException("t_nodes should be even number");
+
+            var graph = new List<int>[](t_nodes);
+            
+        }
+    }
+}
